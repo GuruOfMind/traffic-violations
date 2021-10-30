cd404b3a5f07d6a21873d1fa44ab54c4d1d8202b
Almkdad M. Ali <almkdad.m.ali@gmail.com>
2021-10-30 17:58:27 +0200

    collectstatics

--- trafficviolation/settings.py
+++ trafficviolation/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'trafficviolation.wsgi.application'
 import dj_database_url
 
 db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+# DATABASES['default'].update(db_from_env)
 
 DATABASES = {
     'default': {
