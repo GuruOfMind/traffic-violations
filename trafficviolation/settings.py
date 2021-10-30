db366b337e78dc56e3042011bb5eefcf57434ac9
Almkdad M. Ali <almkdad.m.ali@gmail.com>
2021-10-30 17:45:58 +0200

    add PostegreSQL

--- trafficviolation/settings.py
+++ trafficviolation/settings.py
@@ -88,10 +88,20 @@ WSGI_APPLICATION = 'trafficviolation.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'traffic_violations_db',
+        'USER': 'svu',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
