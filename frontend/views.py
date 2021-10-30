88aa551a65d822ce6112fd21da5e3f6788c08fd8
Almkdad M. Ali <almkdad.m.ali@gmail.com>
2021-10-30 18:47:05 +0200

    change to remote api endpoint

--- frontend/views.py
+++ frontend/views.py
@@ -27,7 +27,7 @@ def Logout_view(request, *args, **kwargs):
 def Regsiter_view(request, *args, **kwargs):
     if request.method == "POST":
         payload = {"plugged_number": request.POST.get("plugged_number"), "driver": request.POST.get("driver"), "category": request.POST.get("category"), "type": request.POST.get("type"), "production_date": request.POST.get("production_date"), "registration_date": request.POST.get("registration_date")}
-        response = requests.post("http://127.0.0.1:8000/api/vehicles/", data=payload)
+        response = requests.post("https://e-traffic-violations.herokuapp.com//api/vehicles/", data=payload)
         if response.status_code == 201:
             data = response.json()
             return redirect("login")
@@ -39,22 +39,22 @@ def Regsiter_view(request, *args, **kwargs):
 @login_required(login_url="/login")
 def Dashboard_view(request, *args, **kwargs):
     current_user = request.user
-    vehicle_url = f"http://127.0.0.1:8000/api/vehicles/{current_user.username}/" 
+    vehicle_url = f"https://e-traffic-violations.herokuapp.com//api/vehicles/{current_user.username}/" 
     user_driver = requests.get(vehicle_url).json()
-    response = requests.get(f"http://127.0.0.1:8000/api/vehicle-violation-logs/?plugged_number={user_driver['plugged_number']}&driver={user_driver['driver']}&is_paid=false")
+    response = requests.get(f"https://e-traffic-violations.herokuapp.com//api/vehicle-violation-logs/?plugged_number={user_driver['plugged_number']}&driver={user_driver['driver']}&is_paid=false")
     violations_list = response.json()
     return render(request, "dashboard.html", {"violations_list": violations_list})
 
 @login_required(login_url="/login")
 def Detail_view(request, id, *args, **kwargs):
-    url = f"http://127.0.0.1:8000/api/vehicle-violation-logs/{id}"
+    url = f"https://e-traffic-violations.herokuapp.com//api/vehicle-violation-logs/{id}"
     response = requests.get(url)
     violation_details = response.json()
     return render(request, "violation.html", {"violation_details": violation_details})
 
 @login_required(login_url="/login")
 def Payment_view(request, id, *args, **kwargs):
-    url = f"http://127.0.0.1:8000/api/vehicle-violation-logs/{id}/"
+    url = f"https://e-traffic-violations.herokuapp.com//api/vehicle-violation-logs/{id}/"
     response = requests.patch(url, data={"is_paid":True})
     if response.status_code == 200:
         messages.add_message(request, messages.INFO, 'Violation has been paid successfully')
